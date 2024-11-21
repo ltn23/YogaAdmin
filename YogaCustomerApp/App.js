ddcd536bab2c12e9547c38a0bfb2489bce4917b5
ltn23 <nhanlt232@gmail.com>
2024-11-21 16:16:26 +0700

    fix navigate

--- YogaCustomerApp/App.js
+++ YogaCustomerApp/App.js
@@ -1,14 +1,4 @@
 import React from 'react';
-import 'react-native-gesture-handler';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 import AppNavigation from './navigate';
 
 const App = () => {
--- YogaCustomerApp/navigate.js
+++ YogaCustomerApp/navigate.js
@@ -1,32 +1,72 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-
+// Import screens
 import HomeScreen from './components/HomeScreen';
 import CartScreen from './components/CartScreen';
 import ClassDetailsScreen from './components/ClassDetailsScreen';
-const Stack = createStackNavigator();
 
+// Create navigators
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// Stack Navigator for Home
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home Screen" component={HomeScreen} />
+      <Stack.Screen name="ClassDetailsScreen" component={ClassDetailsScreen} />
+    </Stack.Navigator>
+  );
+};
+
+// Stack Navigator for Cart
+const CartStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Cart Screen" component={CartScreen} />
+      <Stack.Screen name="ClassDetailsScreen" component={ClassDetailsScreen} />
+    </Stack.Navigator>
+  );
+};
+
+// Main App Navigation
 const AppNavigation = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name="Home Screen"
-                    component={HomeScreen}
-                />
-                <Stack.Screen
-                    name="Cart Screen"
-                    component={CartScreen}
-                />         
-                 <Stack.Screen
-                    name="ClassDetailsScreen"
-                    component={ClassDetailsScreen}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'home-outline';
+            } else if (route.name === 'Cart') {
+              iconName = 'cart-outline';
+            }
+
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#007bff',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeStack}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Cart"
+          component={CartStack}
+          options={{ headerShown: false }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default AppNavigation;
